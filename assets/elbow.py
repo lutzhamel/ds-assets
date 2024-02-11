8151d6d8bc8761be239246bddc737cf226733004
Lutz Hamel <lutz@Lutz-MBP-M1.home>
2024-02-11 09:38:38 -0500

    edits

--- assets/elbow.py
+++ assets/elbow.py
@@ -1,18 +1,18 @@
 # inspired by the source code from
 # www.slideshare.net/SarahGuido/kmeans-clustering-with-scikitlearn
 def plot_elbow(X, n=10):
-    import seaborn as sns
-    sns.set()
-    from matplotlib.pyplot import show, xlabel, ylabel
-    show() # show anything that is already on the display stack.
-    from sklearn.cluster import KMeans
     import numpy as np
-    from scipy.spatial.distance import cdist, pdist
+    import seaborn as sns
+    from matplotlib.pyplot import show, xlabel, ylabel
+    from sklearn.cluster import KMeans
+    from scipy.spatial.distance import cdist
+    sns.set()
+    show() # show anything that is already on the display stack.
     
     # kmeans models for each k
     kMeansModels = [KMeans(n_clusters=k, n_init='auto').fit(X) for k in range(1, n+1)]
     
-    # get the centroids of the models
+    # coordinates of the centroids of the models
     centroids = [m.cluster_centers_ for m in kMeansModels]
     
     # find the distances of the values to the centroids
@@ -21,12 +21,12 @@ def plot_elbow(X, n=10):
     # find the distance of each point to its cluster center
     dist = [np.min(ke, axis=1) for ke in k_euclid]
     
-    # total within cluster sum of squares
-    wcss = [sum(d**2) for d in dist]
+    # average variance for each cluster configuration
+    dist_tuple = zip(list(range(1,n+1)),dist)
+    average_var = [sum(d**2)/k for (k,d) in dist_tuple]
 
     # plot the variance of the models
-    #plt.plot(list(range(1,n+1)),wcss)
-    sns.lineplot(x=list(range(1,n+1)), y=wcss)
+    sns.lineplot(x=list(range(1,n+1)), y=average_var)
     xlabel('k')
-    ylabel('Within Cluster Variance')
+    ylabel('Average Cluster Variance')
     show()
